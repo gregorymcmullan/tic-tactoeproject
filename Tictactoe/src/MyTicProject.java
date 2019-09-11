5a62ee4f1326a5fa0c8ca0e1324af2f96db16700
gregory.mcmullan <gregory.mcmullan@MU-C1-08.mullen.pvt.k12.co.us>
2019-09-11 12:17:59 -0600

    intitial commit

---
+++ Tictactoe/src/MyTicProject.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MyTicProject
+	{
+static String myName;
+		
+		public static void main(String[] args)
+	{
+		Scanner name = new Scanner(System.in);
+		System.out.println("would you like to play a game of tic-tac-toe");
+myName = name 	
+	
+	
+	
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+
